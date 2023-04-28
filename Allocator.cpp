4c639df215638a5455681192ef418152d366cd6b
trhmc <trhmc1811@gmail.com>
2023-04-28 00:30:03 -0400

    created baseline for program

---
+++ Allocator.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class process {
+public:
+    size_t size;
+    pid_t no;
+};
+
+class memory {
+public:
+    size_t size;
+    pid_t no;
+    vector<process> space_occupied;
+
+    void push(const process p) {
+        if (p.size <= size) {
+            space_occupied.push_back(p);
+            size -= p.size;
+        }
+    }
+
+    process pop(const process p) {
+        if (!space_occupied.empty()) {
+            space_occupied.erase(find(space_occupied.begin(), space_occupied.end(), p));
+            return p;
+        }
+    }
+
+    bool empty() {
+        return space_occupied.empty();
+    }
+};
+
+// Implementing strategies
+vector<memory> first_fit(vector<memory> memory_blocks, process p) {
+
+}
+
+vector<memory> best_fit(vector<memory> memory_blocks, process p) {
+    
+}
+
+vector<memory> worst_fit(vector<memory> memory_blocks, process p) {
+    
+}
+// Implementing functions
+void request(vector<memory> memory_blocks, process p) {
+
+}
+
+void release(vector<memory> memory_blocks, process p) {
+
+}
+
+void compact(vector<memory> memory_blocks) {
+
+}
+
+void status(vector<memory> memory_blocks) {
+
+}
+
+int main(int argc, char *argv[]) {
+    // argv should have size 3:
+    // argv[0]: name of program
+    // argv[1]: MAX memory blocks array size
+    // argv[2]: total memory size of memory blocks
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ContiguousMemoryAllocation
+## Initalized memory allocation
+	Max array size and size of memory (example ./allocator 5 10000)
+
+Process class:
+- memory size
+- ID of the process (example: P0)
+
+memory_block class:
+- memory size
+- ID of the memory block
+- array of process occupied
+- pushing a process into the block
+- popping the process off the block
+- bool empty: checking of a block is empty or not
+
+## 4 essential functions:
+1. RQ <process> <memory size> <strategy> - request for a contiguous block of memory
+	- W - worst fit, fitting largest block of memory possible to process
+	- B - best fit, fitting closest size block of memory possible to process
+	- F - first fit, fitting first block of memory of equal or larger size to process
+2. RL <process> - release of a contiguous block of memory
+3. C - compact unused holes of memory into one single block
+4. STAT - report regions of free and allocated memory
+
+main:
+- help menu of function
